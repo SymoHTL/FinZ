a5f584046a56a74a03db05b4706611104af3572e
Symo <54981573+SymoHTL@users.noreply.github.com>
2022-10-11 11:44:11 +0200

    Delete src/minecraft directory

--- src/minecraft/finz/events/impl/EventRender.java
+++
@@ -1,7 +0,0 @@
-package finz.events.impl;
-
-import finz.events.Event;
-
-public class EventRender extends Event {
-
-}
--- src/minecraft/finz/modules/Category.java
+++
@@ -1,5 +0,0 @@
-package finz.modules;
-
-public enum Category {
-    COMBAT, MOVEMENT, PLAYER, MISC, VISUAL, HUD
-}
